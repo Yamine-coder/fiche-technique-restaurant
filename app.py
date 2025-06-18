348d0c848a8bc03ea382d76f79a1a059cb757750
Yaminsight <Moussaouiyamine1@gmail.com>
2025-06-19 01:43:07 +0200

    💄 Refonte affichage KPI : passage au natif Streamlit + responsive

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ with col1:
     st.markdown("Colonne 1")
 with col2:
     st.markdown("Colonne 2")
+
+cols = st.columns(5)
+for i, kpi in enumerate([10,20,30,40,50]):
+    cols[i].metric(f"KPI {i+1}", str(kpi))

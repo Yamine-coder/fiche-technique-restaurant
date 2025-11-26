8fac349e3fa4205687815e292f5c71368b289d72
Yamine-coder <yamine.coder@gmail.com>
2025-11-26 22:50:37 +0100

    Fix: Import edit_dish_view conditionally in app.py instead of __init__.py

--- modules/views/__init__.py
+++ modules/views/__init__.py
@@ -6,11 +6,10 @@ Contient les 4 vues principales : Vue d'ensemble, Analyse d'un plat, Analyse com
 from .overview_view import render_overview_view
 from .dish_analysis_view import render_dish_analysis_view
 from .comparative_view import render_comparative_view
-from .edit_dish_view import render_edit_dish_view
+# edit_dish_view est importé conditionnellement dans app.py
 
 __all__ = [
     "render_overview_view",
     "render_dish_analysis_view", 
     "render_comparative_view",
-    "render_edit_dish_view",
 ]
